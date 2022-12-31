28677c97d46520cc7128c8d25d185ded3d26ec97
ikzekly <ilya.kalinichev@mejuri.com>
2022-12-31 18:59:58 +0100

    instert chunk to list

--- main.c
+++ main.c
@@ -4,14 +4,17 @@
 #include <stdlib.h>
 
 #define HEAP_CAP 640000
-#define HEAP_ALLOCED_CAP 1024
-#define HEAP_FREED_CAP 1024
+#define CHUNK_LIST_CAP 1024
 
-#define NOT_IMPLEMENTED \
-    do { \
-        fprintf(stderr, "%s:%d: TODO: %s is not implemented yet\n", __FILE__, __LINE__, __func__); \
-        abort(); \
-    } while(0)
+#define NOT_IMPLEMENTED                                                        \
+    do                                                                         \
+    {                                                                          \
+        fprintf(                                                               \
+            stderr, "%s:%d: TODO: %s is not implemented yet\n", __FILE__,      \
+            __LINE__, __func__                                                 \
+        );                                                                     \
+        abort();                                                               \
+    } while (0)
 
 typedef struct
 {
@@ -22,45 +25,52 @@ typedef struct
 typedef struct
 {
     size_t count;
-    Chunk chunks[HEAP_ALLOCED_CAP];
+    Chunk chunks[CHUNK_LIST_CAP];
 } Chunk_List;
 
-void chunk_list_dump(const Chunk_List *list)
+void chunk_list_dump(const Chunk_List* list)
 {
     printf("Chunks (%zu):\n", list->count);
 
     for (size_t i = 0; i < list->count; ++i)
     {
         printf(
-            "  start: %p, size: %zu\n",
-	    list->chunks[i].start,
+            "  start: %p, size: %zu\n", list->chunks[i].start,
             list->chunks[i].size
         );
     }
 }
 
-int chunk_list_find(const Chunk_List *list, void* ptr)
+int chunk_list_find(const Chunk_List* list, void* start)
 {
-    (void) list;
-    (void) ptr;
+    (void)list;
+    (void)start;
 
     NOT_IMPLEMENTED;
     return -1;
 }
 
-void chunk_list_insert(const Chunk_List *list, void* ptr, size_t size)
+void chunk_list_insert(Chunk_List* list, void* start, size_t size)
 {
-    (void) list;
-    (void) ptr;
-    (void) size;
+    assert(list->count < CHUNK_LIST_CAP);
+    list->chunks[list->count].start = start;
+    list->chunks[list->count].size = size;
 
-    NOT_IMPLEMENTED;
+    for (size_t i = list->count;
+         i > 0 && list->chunks[i].start < list->chunks[i - 1].start; --i)
+    {
+        const Chunk t = list->chunks[i];
+        list->chunks[i] = list->chunks[i - 1];
+        list->chunks[i - 1] = t;
+    }
+
+    list->count += 1;
 }
 
-void chunk_list_remove(Chunk_List *list, size_t index)
+void chunk_list_remove(Chunk_List* list, size_t index)
 {
-    (void) list;
-    (void) index;
+    (void)list;
+    (void)index;
 
     NOT_IMPLEMENTED;
 }
@@ -80,25 +90,22 @@ void* heap_alloc(size_t size)
     };
 
     assert(heap_size + size <= HEAP_CAP);
-    void* ptr = heap + heap_size;
+    void* start = heap + heap_size;
     heap_size += size;
 
-    chunk_list_insert(&alloced_chunks, ptr, size);
+    chunk_list_insert(&alloced_chunks, start, size);
 
-    return ptr;
+    return start;
 }
 
-void heap_free(void* ptr)
+void heap_free(void* start)
 {
-    (void) ptr;
+    (void)start;
 
     NOT_IMPLEMENTED;
 }
 
-void heap_collect()
-{
-    NOT_IMPLEMENTED;
-}
+void heap_collect() { NOT_IMPLEMENTED; }
 
 int main()
 {
