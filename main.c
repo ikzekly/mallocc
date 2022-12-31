7d517c91b3f7a8d2e6a641e776ff78e16a87bfe0
ikzekly <ilya.kalinichev@mejuri.com>
2022-12-31 02:30:39 +0100

    allocate in chunks

--- main.c
+++ main.c
@@ -2,33 +2,72 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define HEAP_CAPACITY 640000
+#define HEAP_CAP 640000
+#define HEAP_ALLOCED_CAP 1024
 
-char heap[HEAP_CAPACITY] = {0};
+typedef struct
+{
+    void* start;
+    size_t size;
+} Heap_Chunk;
+
+char heap[HEAP_CAP] = {0};
 size_t heap_size = 0;
 
-void *heap_alloc(size_t size) {
-    assert(heap_size + size <= HEAP_CAPACITY);
-    void *result = heap + heap_size;
+Heap_Chunk heap_alloced[HEAP_ALLOCED_CAP] = {0};
+size_t heap_alloced_size = 0;
+
+void* heap_alloc(size_t size)
+{
+    assert(heap_size + size <= HEAP_CAP);
+    void* result = heap + heap_size;
     heap_size += size;
+
+    const Heap_Chunk chunk = {
+        .start = result,
+        .size = size,
+    };
+    assert(heap_alloced_size < HEAP_ALLOCED_CAP);
+    heap_alloced[heap_alloced_size++] = chunk;
 
     return result;
 }
 
-void heap_free(void *ptr) {
+void heap_dump_alloced_chunks(void)
+{
+    printf("Allocated Chunks (%zu):\n", heap_alloced_size);
+
+    for (size_t i = 0; i < heap_alloced_size; ++i)
+    {
+        printf(
+            "  start: %p, size: %zu\n", heap_alloced[i].start,
+            heap_alloced[i].size
+        );
+    }
+}
+
+void heap_free(void* ptr)
+{
     assert(false && "TODO: heap_free is not implemented");
     (void)ptr;
 }
 
-void heap_collect() {
+void heap_collect()
+{
     assert(false && "TODO: heap_collect is not implemented");
 }
 
-int main() {
-    char *root = heap_alloc(26);
-    for (int i = 0; i < 26; ++i) {
+int main()
+{
+    char* root = heap_alloc(26);
+    for (int i = 0; i < 26; ++i)
+    {
         root[i] = i + 'A';
     }
+
+    heap_dump_alloced_chunks();
+
+    heap_free(root);
 
     return 0;
 }
