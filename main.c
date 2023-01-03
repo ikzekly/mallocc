57e5cfed3c7a14cc10145783937a304b0802caf3
ikzekly <ilya.kalinichev@mejuri.com>
2023-01-03 21:27:29 +0100

    merge freed chunks after use

--- main.c
+++ main.c
@@ -28,6 +28,51 @@ typedef struct
     Chunk chunks[CHUNK_LIST_CAP];
 } Chunk_List;
 
+void chunk_list_insert(Chunk_List* list, void* start, size_t size)
+{
+    assert(list->count < CHUNK_LIST_CAP);
+    list->chunks[list->count].start = start;
+    list->chunks[list->count].size = size;
+
+    for (size_t i = list->count;
+         i > 0 && list->chunks[i].start < list->chunks[i - 1].start; --i)
+    {
+        const Chunk t = list->chunks[i];
+        list->chunks[i] = list->chunks[i - 1];
+        list->chunks[i - 1] = t;
+    }
+
+    list->count += 1;
+}
+
+void chunk_list_merge(Chunk_List* dst, const Chunk_List* src)
+{
+    dst->count = 0;
+
+    for (size_t i = 0; i < src->count; ++i)
+    {
+        const Chunk chunk = src->chunks[i];
+
+        if (dst->count > 0)
+        {
+            Chunk* top_chunk = &dst->chunks[dst->count - 1];
+
+            if (top_chunk->start + top_chunk->size == chunk.start)
+            {
+                top_chunk->size += chunk.size;
+            }
+            else
+            {
+                chunk_list_insert(dst, chunk.start, chunk.size);
+            }
+        }
+        else
+        {
+            chunk_list_insert(dst, chunk.start, chunk.size);
+        }
+    }
+}
+
 void chunk_list_dump(const Chunk_List* list)
 {
     printf("Chunks (%zu):\n", list->count);
@@ -69,23 +114,6 @@ int chunk_list_find(const Chunk_List* list, void* start)
     }
 }
 
-void chunk_list_insert(Chunk_List* list, void* start, size_t size)
-{
-    assert(list->count < CHUNK_LIST_CAP);
-    list->chunks[list->count].start = start;
-    list->chunks[list->count].size = size;
-
-    for (size_t i = list->count;
-         i > 0 && list->chunks[i].start < list->chunks[i - 1].start; --i)
-    {
-        const Chunk t = list->chunks[i];
-        list->chunks[i] = list->chunks[i - 1];
-        list->chunks[i - 1] = t;
-    }
-
-    list->count += 1;
-}
-
 void chunk_list_remove(Chunk_List* list, size_t index)
 {
     assert(index < list->count);
@@ -105,12 +133,17 @@ Chunk_List freed_chunks = {
     .chunks = {[0] = {.start = heap, .size = sizeof(heap)}},
 };
 
+Chunk_List tmp_chunks = {0};
+
 void* heap_alloc(size_t size)
 {
     if (size <= 0)
     {
         return NULL;
     };
+
+    chunk_list_merge(&tmp_chunks, &freed_chunks);
+    freed_chunks = tmp_chunks;
 
     for (size_t i = 0; i < freed_chunks.count; ++i)
     {
@@ -160,11 +193,12 @@ int main()
     for (int i = 0; i < 10; ++i)
     {
         void* p = heap_alloc(i);
-        if (i % 2 == 0)
-        {
+        if (i % 2 == 0) {
             heap_free(p);
         }
     }
+
+    heap_alloc(10);
 
     chunk_list_dump(&alloced_chunks);
     chunk_list_dump(&freed_chunks);
